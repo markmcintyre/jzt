f21b45bb45fdcd8a7289e0adb45bad18ba1fc443
Mark McIntyre <mark.mcintyre@onethousand.ca>
2013-03-02 11:19:28 -0500

    Updated the documentation on things.js

--- script/things.js
+++ script/things.js
@@ -2,7 +2,14 @@ window.jzt = window.jzt || {};
 jzt.things = jzt.things || {};
 
 /*
- * Thing
+ * Thing represents a single 'thing' that can be associated with a Board. This can
+ * range from players, to walls, to ScriptableThings. Each Thing has a location,
+ * sprite index, and foreground and background color. Each Thing should also
+ * be able to handle its own serialization and deserialization. Things may also
+ * receive messages and take actions accordingly, either altering its own state
+ * or making requests to its owner Board.
+ *
+ * @param board A Board instance to "own" this Thing.
  */
 jzt.things.Thing = function(board) {
     this.name = undefined;
@@ -13,6 +20,11 @@ jzt.things.Thing = function(board) {
     this.background = jzt.colors.Colors['0'];
 };
 
+/**
+ * Serializes this Thing to an object and returns it.
+ *
+ * @return A serialized version of this Thing.
+ */
 jzt.things.Thing.prototype.serialize = function() {
     var result = {};
     result.name = this.name;
@@ -23,6 +35,12 @@ jzt.things.Thing.prototype.serialize = function() {
     return result;
 };
 
+/**
+ * Deserializes a given data object and update's this Thing's state to
+ * that data.
+ *
+ * @param data An object to be deserialized into this Thing.
+ */
 jzt.things.Thing.prototype.deserialize = function(data) {
     
     this.name = data.name;
@@ -38,22 +56,48 @@ jzt.things.Thing.prototype.deserialize = function(data) {
 
 };
 
+/**
+ * Delivers a provided message to this Thing.
+ *
+ * @param messageName a name of a message to deliver.
+ */
 jzt.things.Thing.prototype.sendMessage = function(messageName) {};
 
+/**
+ * Retrurns whether or not this Thing declares itself to be pushable in a provided
+ * direction by other Things.
+ *
+ * @param direction A direction in which to test the pushability of this Thing
+ * @return True if this thing is pushable in a given direction, false otherwise.
+ */
 jzt.things.Thing.prototype.isPushable = function(direction) {
     return false;
 };
 
+/**
+ * Moves this Thing in a provided Direction and returns its success.
+ * 
+ * @param direction A Direction in which to move this Thing.
+ * @return true if the move was successful, false otherwise.
+ */
 jzt.things.Thing.prototype.move = function(direction) {
     return this.board.moveTile(this.point, this.point.add(direction));
 };
 
+/**
+ * Removes this Thing from its owner board.
+ */
 jzt.things.Thing.prototype.delete = function() {
     this.board.deleteTile(this.point);
 };
 
+// ------------------------------------------------------------------------------
+
 /*
- * Updateable Thing
+ * Updateable Thing is a Thing that can be updated during an execution cycle 
+ * of its owner board. UpdateableThings may take different actions during updates.
+ * 
+ * @param board A Board to which this UpdateableThing belongs.
  */
 jzt.things.UpdateableThing = function(board) {
     jzt.things.Thing.call(this, board);
@@ -62,27 +106,56 @@ jzt.things.UpdateableThing = function(board) {
 jzt.things.UpdateableThing.prototype = new jzt.things.Thing();
 jzt.things.UpdateableThing.prototype.constructor = jzt.things.UpdateableThing;
 
+/**
+ * Serializes this Thing to an object and returns it.
+ *
+ * @return A serialized version of this Thing.
+ */
 jzt.things.UpdateableThing.prototype.serialize = function() {
     var result = jzt.things.Thing.prototype.serialize.call(this) || {};
     result.speed = this.speed;
     return result;
 };
 
+/**
+ * Deserializes a given data object and update's this Thing's state to
+ * that data.
+ *
+ * @param data An object to be deserialized into this Thing.
+ */
 jzt.things.UpdateableThing.prototype.deserialize = function(data) {
     jzt.things.Thing.prototype.deserialize.call(this, data);
     this.setSpeed(data.speed);
 };
 
+/**
+ * Assigns an update speed to this UpdateableThing. Things will only
+ * execute their update logic at their own defined speed.
+ *
+ * @param A speed (in updates per second) at which this UpdateableThing
+ *        will update itself.
+ */
 jzt.things.UpdateableThing.prototype.setSpeed = function(speed) {
     this.speed = speed;
     this.ticksPerCycle = Math.round(1000 / this.speed);
     this.nextTick = Date.now() + this.ticksPerCycle;
 };
 
+/**
+ * Retrieves a Direction toward this UpdateableThing's Board's Player.
+ *
+ * @return a Direction toward a Player.
+ */
 jzt.things.UpdateableThing.prototype.getPlayerDirection = function() {
     return this.point.directionTo(this.board.player.point);
 };
 
+/**
+ * Retrievs an Array of Directions in which this UpdateableThing is
+ * free to move on its owner Board.
+ *
+ * @return An array of Directions.
+ */
 jzt.things.UpdateableThing.prototype.getFreeDirections = function() {
     
     var result = [];
@@ -98,6 +171,12 @@ jzt.things.UpdateableThing.prototype.getFreeDirections = function() {
 
 };
 
+/**
+ * Retrieves an Array of Directions in which this UpdateableThing is not free
+ * to move on its owner Board.
+ *
+ * @return An array of Directions.
+ */
 jzt.things.UpdateableThing.prototype.getBlockedDirections = function() {
   
     var result = [];
@@ -113,10 +192,19 @@ jzt.things.UpdateableThing.prototype.getBlockedDirections = function() {
  
 };
 
+/**
+ * Updates this UpdateableThing for a single execution cycle.
+ */
 jzt.things.UpdateableThing.prototype.update = function() {};
 
+// ------------------------------------------------------------------------------
+
 /*
- * Scriptable Thing
+ * Scriptable Thing is an UpdateableThing capable of executing a Script instance
+ * for its update cycles. This Script will be updated in a ScriptContext unique
+ * to this ScriptableThing.
+ *
+ * @param board An owner board for this ScriptableThing.
  */
 jzt.things.ScriptableThing = function(board) {
     jzt.things.UpdateableThing.call(this, board);
@@ -129,6 +217,11 @@ jzt.things.ScriptableThing = function(board) {
 jzt.things.ScriptableThing.prototype = new jzt.things.UpdateableThing();
 jzt.things.ScriptableThing.prototype.constructor = jzt.things.ScriptableThing;
 
+/**
+ * Serializes this Thing to an object and returns it.
+ *
+ * @return A serialized version of this Thing.
+ */
 jzt.things.ScriptableThing.prototype.serialize = function() {
     var result = jzt.things.UpdateableThing.prototype.serialize.call(this) || {};
 
@@ -146,6 +239,12 @@ jzt.things.ScriptableThing.prototype.serialize = function() {
 
 };
 
+/**
+ * Deserializes a given data object and update's this Thing's state to
+ * that data.
+ *
+ * @param data An object to be deserialized into this Thing.
+ */
 jzt.things.ScriptableThing.prototype.deserialize = function(data) {
     jzt.things.UpdateableThing.prototype.deserialize.call(this, data);
     this.scriptName = data.script;
@@ -158,6 +257,13 @@ jzt.things.ScriptableThing.prototype.deserialize = function(data) {
     }
 };
 
+/**
+ * Delivers a message to this ScriptableThing. This message will be passed 
+ * on to its Script during its next execution cycle if this ScriptableInstance
+ * is not in a locked state.
+ *
+ * @param message A message to be delivered to this ScriptableThing.
+ */
 jzt.things.ScriptableThing.prototype.sendMessage = function(message) {
     
     // If we are ready to receive a message...
@@ -172,11 +278,23 @@ jzt.things.ScriptableThing.prototype.sendMessage = function(message) {
     
 };
 
+/**
+ * Moves this Thing in a provided Direction and returns its success.
+ * 
+ * @param direction A Direction in which to move this Thing.
+ * @return true if the move was successful, false otherwise.
+ */
 jzt.things.ScriptableThing.prototype.move = function(direction) {
     this.orientation = direction;
     return jzt.things.UpdateableThing.prototype.move.call(this, direction);
 };
 
+/**
+ * Makes this ScriptableThing walk in its current walking direction.
+ * ScriptableThings can walk while executing other instructions. If this
+ * ScriptableThing walks into an obsticle and cannot continue walking,
+ * it will receive a 'THUD' message and halt its walking direction.
+ */
 jzt.things.ScriptableThing.prototype.walk = function() {
     if(this.walkDirection) {
         
@@ -190,6 +308,10 @@ jzt.things.ScriptableThing.prototype.walk = function() {
     }
 };
 
+/**
+ * Updates this ScriptableThing for a single execution cycle, including its
+ * associated Script.
+ */
 jzt.things.ScriptableThing.prototype.update = function() {
     
     var now = Date.now();
@@ -202,9 +324,9 @@ jzt.things.ScriptableThing.prototype.update = function() {
     
 };
 
-/*===============================================================
+/*==================================================================================
  * BUILT-IN THINGS
- *==============================================================*/
+ *=================================================================================*/
  
 /*
  * Boulder
