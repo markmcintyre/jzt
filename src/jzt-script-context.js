3c203e63fe6311a1c969b90f517d12d1ada3d8c1
Mark McIntyre <mark.mcintyre@onethousand.ca>
2017-02-09 20:44:46 -0500

    Fixed an issue where jumping to a non-existant label would crash the game.

--- src/jzt-script-context.js
+++ src/jzt-script-context.js
@@ -289,7 +289,17 @@ JztScriptContext.prototype.jumpToLabel = function (label) {
             // Increment our jump count for this tick
             this.jumpCount += 1;
 
+        } else {
+
+            // All the labels were zapped, so stop.
+            this.stop();
+
         }
+
+    } else {
+
+        // There was no label to jump to, so stop.
+        this.stop();
 
     }
 
